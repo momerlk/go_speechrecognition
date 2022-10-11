257e6be63340286baa0ae16b2bc01c6f3f9240e0
Omer Ali Malik <omeralimalik96@gmail.com>
2022-10-11 22:39:17 +0500

    Breaking changes; Barely working just a simple recognizer

--- go.mod
+++ go.mod
@@ -5,15 +5,13 @@ go 1.16
 require (
 	github.com/IBM/go-sdk-core/v5 v5.9.1
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
-	github.com/dragonmaster101/go_audio v0.2.1 // indirect
 	github.com/go-openapi/errors v0.20.1 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/google/go-cmp v0.5.5 // indirect
-	github.com/gorilla/websocket v1.4.2
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
-	github.com/sacOO7/gowebsocket v0.0.0-20210515122958-9396f1a71e23 // indirect
+	github.com/sacOO7/gowebsocket v0.0.0-20210515122958-9396f1a71e23
 	github.com/watson-developer-cloud/go-sdk/v2 v2.2.0
 	go.mongodb.org/mongo-driver v1.8.2 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
--- speechrecognition/speechrecogntion.go
+++ speechrecognition/speechrecogntion.go
@@ -1,53 +1,19 @@
 package speechrecognition
 
 import (
-	"fmt"
-	"log"
-	"os"
 	"strings"
 
-	audio "github.com/dragonmaster101/go_audio/audio"
-	capture "github.com/dragonmaster101/go_speechrecognition/capture"
+
 	watson "github.com/dragonmaster101/go_speechrecognition/watson_sr"
 )
 
 type recognizer interface {
 	TranscribeFile(fileName string) (string , error)
-	AsyncTranscribe(res chan string , mic Microphone , done chan bool)
+
 }
 type Recognizer recognizer
 
-type Source interface {
-	GetData() *os.File
-}
 
-type Microphone interface {
-	Record(fileName string , timeLimit int)
-}
-
-func NewMicrophone() Microphone{
-	mic := &portAudioMicrophone{};
-	mic.Init();
-
-	return mic;
-}
-
-// uses a microphone to Recognize audio and then transcribes it using the recognizer
-func Recognize(recognizer Recognizer , mic Microphone) string {
-	recordingFile := "recording101.aiff";
-	mic.Record(recordingFile , 4);
-	
-	res , err := recognizer.TranscribeFile(recordingFile);
-	if err != nil {
-		log.Fatal(err);
-	}
-
-	return res;
-}
-
-func AsyncRecognize(recognizer Recognizer , mic Microphone , res chan string , done chan bool){
-	recognizer.AsyncTranscribe(res , mic , done);
-}
 
 /*
 <---------------------------------------------------------------------------------------------------------------------->
@@ -57,22 +23,6 @@ func AsyncRecognize(recognizer Recognizer , mic Microphone , res chan string , d
 
 type RecognizerWatson struct {
 	watson.SpeechToTextService
-}
-
-func (r *RecognizerWatson) AsyncTranscribe(resChannel chan string , mic Microphone , done chan bool) {
-	r.SocketsTranscribe(
-		func(socket watson.Socket , state *int) {
-
-			fmt.Println("Recording! Say Something");
-			sourceFileName := "recording.aiff"
-			capture.CaptureWithCGo(func(){mic.Record(sourceFileName , 4)});
-			fmt.Println("Recording Complete!");
-			watson.SendSocketRecognitionRequest(socket , sourceFileName , state);
-
-		}, 
-		func (result string) {
-			resChannel <- result;
-		} , done);
 }
 
 // Creates A Service instance from the Watson Speech To Text V1 sdk
@@ -100,34 +50,3 @@ func RecognizerIBM(API_KEY , API_URL string) Recognizer {
 	return recognizer;
 }
 
-// uses the portaudio library to record '.aiff' files from the microphone PORTAUDIO SHOULD BE INSTALLED
-type portAudioMicrophone struct {
-	recorded bool
-	fileName string
-}
-
-func (p *portAudioMicrophone) Init(){
-	p.recorded = false;
-}
-
-func (p *portAudioMicrophone) Record(fileName string , timeLimit int){
-	p.recorded = true;
-	p.fileName = fileName;
-	audio.Record(fileName , timeLimit);
-} 
-
-func (p *portAudioMicrophone) GetData() *os.File{
-	
-	if !p.recorded {
-		p.fileName = "mic101.aiff"
-		p.Record(p.fileName , 4);
-	}
-
-	file , err := os.Open(p.fileName);
-	if err != nil {
-		log.Fatal(err);
-	}
-	defer file.Close();
-
-	return file;
-}
